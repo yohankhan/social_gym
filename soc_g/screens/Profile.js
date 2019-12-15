07b38e21b9020b5feebe291bf75bc5d3d82ba1d0
yohan <yohank786@gmail.com>
2019-12-15 18:15:04 +0530

    Profile UI

--- soc_g/screens/Profile.js
+++ soc_g/screens/Profile.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button, Dimensions } from 'react-native';
+import bd from '../assets/bd.png';
+
+const W = Dimensions.get('window');
 
 class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <View>
-        <Text> Profile </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={{ height: W.height / 2 }}>
+				<View style={{ flex: 1, flexDirection: 'column', alignItems: 'center'}}>
+					<View
+						style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}
+					>
+						<Text>FOLLOWING</Text>
+						<Image source={bd} style={{ width: 80, height: 100, borderRadius: 30 }} />
+						<Text>FOLLOWERS</Text>
+					</View>
+					<Text style={{ alignItems: 'center' }}>@USERNAME</Text>
+					<View
+						style={{ flex: 1, flexDirection: 'row', justifyContent: "space-evenly", alignItems: 'center' }}
+					>
+						<Button title="UPLOAD" />
+						<Button title="EDIT" />
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
 
 export default Profile;
